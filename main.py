1da577f0973dc26276c3a0521e2727ed9abc80f7
davidrey123 <davidrey.maths@gmail.com>
2024-06-14 11:09:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,36 +2,25 @@
 from src import Network
 from src import Leblanc
 from src import FS_NETS
-from src import BC
-from src import BPC
 
 net = 'SiouxFalls'
 ins = 'SF_DNDP_10_1'
-'''
-network = Network.Network(net,ins,0.5,1e-0,1e-3)
-print(net,ins)
-bc = BC.BC(network)
-bc.BB()
-print()
-'''
 
 network = Network.Network(net,ins,0.5,1e-0,1e-3)
 print(net,ins)
-bpc = BPC.BPC(network)
-bpc.BB()
-print()
 
-'''
-network = Network.Network(net,ins,0.5,1e-0,1e-3)
-print(net,ins)
+y = {}
+for a network.links2
+  y[a] = 0
+  lbd[a] = 0
+
+tstt = network.msa(y, lbd)
+print(tstt)
+
+#---solve DNDP using Leblanc's BB algorithm
 leblanc = Leblanc.Leblanc(network)
 leblanc.BB()
-print()
-'''
-'''
-network = Network.Network(net,ins,0.5,1e-0,1e-3)
-print(net,ins)
+
+#---solve DNDP using FS NETS's BB algorithm
 fs_nets = FS_NETS.FS_NETS(network)
 fs_nets.BB()
-print()
-'''
