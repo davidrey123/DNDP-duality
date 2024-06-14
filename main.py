9b87da2bf7bafb8ba917e10ece7bf4bada2bdabc
davidrey123 <davidrey.maths@gmail.com>
2024-06-14 11:06:35 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+#---modules
+from src import Network
+from src import Leblanc
+from src import FS_NETS
+from src import BC
+from src import BPC
+
+net = 'SiouxFalls'
+ins = 'SF_DNDP_10_1'
+'''
+network = Network.Network(net,ins,0.5,1e-0,1e-3)
+print(net,ins)
+bc = BC.BC(network)
+bc.BB()
+print()
+'''
+
+network = Network.Network(net,ins,0.5,1e-0,1e-3)
+print(net,ins)
+bpc = BPC.BPC(network)
+bpc.BB()
+print()
+
+'''
+network = Network.Network(net,ins,0.5,1e-0,1e-3)
+print(net,ins)
+leblanc = Leblanc.Leblanc(network)
+leblanc.BB()
+print()
+'''
+'''
+network = Network.Network(net,ins,0.5,1e-0,1e-3)
+print(net,ins)
+fs_nets = FS_NETS.FS_NETS(network)
+fs_nets.BB()
+print()
+'''
